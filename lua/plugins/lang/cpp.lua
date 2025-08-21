f583d31d9a243ea143491ec11b34db10d2823eca
arch <arch>
2025-08-21 19:57:23 +0800

    support c++

---
+++ lua/plugins/lang/cpp.lua
@@ -0,0 +1,58 @@
+return {
+  -- lsp config for c++
+  {
+    "neovim/nvim-lspconfig",
+    ft = { "c", "cpp", "objc", "objcpp" },
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.clangd.setup({
+        cmd = { "clangd", "--background-index" },
+        filetypes = { "c", "cpp", "objc", "objcpp" },
+        root_dir = lspconfig.util.root_pattern("compile_commands.json", "compile_flags.txt", ".git"),
+        capabilities = (function()
+          local ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+          if ok then
+            return cmp_nvim_lsp.default_capabilities()
+          else
+            return vim.lsp.protocol.make_client_capabilities()
+          end
+        end)(),
+        on_attach = function(client, bufnr)
+          vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+          -- 格式化快捷键 <leader>cf
+          vim.keymap.set("n", "<leader>cf", function()
+            vim.lsp.buf.format({ async = true })
+          end, { buffer = bufnr, desc = "Format C++ code" })
+        end,
+      })
+      -- 自动保存时格式化
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = { "*.c", "*.cpp", "*.h", "*.hpp" },
+        callback = function()
+          vim.lsp.buf.format({ async = false })
+        end,
+      })
+    end,
+  },
+
+  -- 自动补全源（nvim-cmp 和 cmp-nvim-lsp）
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = { "hrsh7th/cmp-nvim-lsp" },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+      })
+    end,
+  },
+
+  { "hrsh7th/cmp-nvim-lsp" },
+}
