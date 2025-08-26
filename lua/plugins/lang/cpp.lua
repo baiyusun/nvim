7d8d38238bd7ef0c2e1fa11cacae6292a9fecccf
craft <craft>
2025-08-26 20:50:26 +0800

    update config of python & c++

--- lua/plugins/lang/cpp.lua
+++ lua/plugins/lang/cpp.lua
@@ -1,31 +1,16 @@
 return {
-  -- lsp config for c++
   {
     "neovim/nvim-lspconfig",
     ft = { "c", "cpp", "objc", "objcpp" },
+    dependencies = { "hrsh7th/nvim-cmp", "hrsh7th/cmp-nvim-lsp" },
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.clangd.setup({
-        cmd = { "clangd", "--background-index" },
-        filetypes = { "c", "cpp", "objc", "objcpp" },
-        root_dir = lspconfig.util.root_pattern("compile_commands.json", "compile_flags.txt", ".git"),
-        capabilities = (function()
-          local ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-          if ok then
-            return cmp_nvim_lsp.default_capabilities()
-          else
-            return vim.lsp.protocol.make_client_capabilities()
-          end
-        end)(),
+        capabilities = require("cmp_nvim_lsp").default_capabilities(),
         on_attach = function(client, bufnr)
           vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-          -- 格式化快捷键 <leader>cf
-          vim.keymap.set("n", "<leader>cf", function()
-            vim.lsp.buf.format({ async = true })
-          end, { buffer = bufnr, desc = "Format C++ code" })
         end,
       })
-      -- 自动保存时格式化
       vim.api.nvim_create_autocmd("BufWritePre", {
         pattern = { "*.c", "*.cpp", "*.h", "*.hpp" },
         callback = function()
@@ -34,6 +19,4 @@ return {
       })
     end,
   },
-
-  { "hrsh7th/cmp-nvim-lsp" },
 }
--- lua/plugins/lang/python.lua
+++ lua/plugins/lang/python.lua
@@ -1,113 +1,22 @@
-vim.lsp.enable("basedpyright")
-
 return {
   {
-    "nvim-treesitter/nvim-treesitter",
-    optional = true,
-    opts = {
-      ensure_installed = { "ninja", "rst", "python" },
-    },
-    opts_extend = { "ensure_installed" },
-  },
-  {
-    "williamboman/mason.nvim",
-    optional = true,
-    opts = {
-      ensure_installed = {
-        "basedpyright",
-        "pyright",
-        "black",
-        "isort",
-      },
-    },
-    opts_extend = { "ensure_installed" },
-  },
-  {
-    "stevearc/conform.nvim",
-    optional = true,
-    opts = {
-      formatters_by_ft = {
-        python = { "black", "isort" },
-      },
-    },
-    opts_extend = { "ensure_installed" },
-  },
-  -- Python 调试支持
-  {
-    "mfussenegger/nvim-dap-python",
-    ft = "python",
-    dependencies = {
-      "mfussenegger/nvim-dap",
-      "rcarriga/nvim-dap-ui",
-    },
+    "neovim/nvim-lspconfig",
+    ft = { "python" },
+    dependencies = { "hrsh7th/nvim-cmp", "hrsh7th/cmp-nvim-lsp" }, -- 加入依赖，确保加载顺序
     config = function()
-      local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-      require("dap-python").setup(path)
-      vim.keymap.set("n", "<leader>dpr", function()
-        require("dap-python").test_method()
-      end, { desc = "调试 Python 方法" })
+      local lspconfig = require("lspconfig")
+      lspconfig.pyright.setup({
+        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+        on_attach = function(client, bufnr)
+          vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+        end,
+      })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = { "*.py" },
+        callback = function()
+          vim.lsp.buf.format({ async = false })
+        end,
+      })
     end,
   },
-
-  -- 通用调试适配器
-  {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-      "rcarriga/nvim-dap-ui",
-      { "nvim-neotest/nvim-nio" },
-      "theHamsta/nvim-dap-virtual-text",
-    },
-    config = function()
-      local dap = require("dap")
-      local dapui = require("dapui")
-
-      dapui.setup()
-      require("nvim-dap-virtual-text").setup()
-
-      -- 调试快捷键
-      vim.keymap.set("n", "<F5>", function()
-        dap.continue()
-      end, { desc = "开始/继续调试" })
-      vim.keymap.set("n", "<F10>", function()
-        dap.step_over()
-      end, { desc = "单步跳过" })
-      vim.keymap.set("n", "<F11>", function()
-        dap.step_into()
-      end, { desc = "单步进入" })
-      vim.keymap.set("n", "<F12>", function()
-        dap.step_out()
-      end, { desc = "单步退出" })
-      vim.keymap.set("n", "<leader>b", function()
-        dap.toggle_breakpoint()
-      end, { desc = "切换断点" })
-      vim.keymap.set("n", "<leader>B", function()
-        dap.set_breakpoint(vim.fn.input("断点条件: "))
-      end, { desc = "条件断点" })
-      vim.keymap.set("n", "<leader>dr", function()
-        dap.repl.open()
-      end, { desc = "打开调试 REPL" })
-      vim.keymap.set("n", "<leader>dl", function()
-        dap.run_last()
-      end, { desc = "运行上次调试" })
-
-      -- 自动打开/关闭 DAP UI
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
-    end,
-  },
-
-  -- 代码补全
-  { "hrsh7th/nvim-cmp" },
-  { "hrsh7th/cmp-buffer" },
-  { "hrsh7th/cmp-path" },
-  { "saadparwaiz1/cmp_luasnip" },
-  { "hrsh7th/cmp-nvim-lsp" },
-  { "hrsh7th/cmp-nvim-lua" },
 }
